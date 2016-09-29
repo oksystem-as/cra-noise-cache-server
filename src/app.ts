e7eb35a7b2af3ba7ac7fad6928eb1ac61653b530
Milan Kacálek <kacalek@oksystem.cz>
2016-09-29 09:35:55 +0200

    Přidáno Access-Control-Allow-Origin *

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,12 @@ class Server {
 
     this.app.use(morgan(":remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" " +
                         ":status :res[content-length] :response-time ms \":referrer\" \":user-agent\""));
-
+    this.app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     this.configCache();
     this.routes();
   }
