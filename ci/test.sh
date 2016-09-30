56b0c4e99646b4a51e9ea4dd56ebf7ce1772ca93
Milan Kacálek <kacalek@oksystem.cz>
2016-09-30 13:22:15 +0200

    Přidany testy.

--- ci/test.sh
+++ ci/test.sh
@@ -5,7 +5,7 @@ set -e -x
 apk add --no-cache curl
 npm install -g json-diff
 
-curl "http://hndocker.oksystem.local:58081/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o a.json
+curl "https://api.pripoj.me/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o a.json
 curl "http://hndocker.oksystem.local:58081/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o b.json
 
 sh ./tmp/test/diff.sh a.json b.json
