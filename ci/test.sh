49736f4c144eb7f13179551ba809e353fe70adda
Milan Kacálek <kacalek@oksystem.cz>
2016-09-30 12:52:06 +0200

    Přidany testy.

--- ci/test.sh
+++ ci/test.sh
@@ -8,4 +8,4 @@ npm install -f json-diff
 curl "http://hndocker.oksystem.local:58081/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o a.json
 curl "http://hndocker.oksystem.local:58081/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o b.json
 
-sh ./diff.sh a.json b.json
+sh ./tmp/test/diff.sh a.json b.json
