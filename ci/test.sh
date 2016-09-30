88759402d3d56c698240daa9f36706a2cbeab8cc
Milan Kacálek <kacalek@oksystem.cz>
2016-09-30 13:34:38 +0200

    Přidany testy.

--- ci/test.sh
+++ ci/test.sh
@@ -6,6 +6,6 @@ apk add --no-cache curl
 npm install -g json-diff
 
 curl "https://api.pripoj.me/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o a.json
-curl "http://localhost:8080/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o b.json
+curl "http://127.0.0.1:8080/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o b.json
 
 sh ./tmp/test/diff.sh a.json b.json
