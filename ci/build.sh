170617ab3335e0faa2d313c266abee66c3b7e1a5
Milan Kacálek <kacalek@oksystem.cz>
2016-09-30 07:01:08 +0200

    CI

--- ci/build.sh
+++ ci/build.sh
@@ -7,3 +7,8 @@ npm install
 npm run typings install
 npm run grunt
 ls -l
+cp ./bin ../binaries/
+cp ./docs ../binaries/
+cp ./config.yaml ../binaries/
+cp ./package.json ../binaries/
+cp ./dist ../binaries/
