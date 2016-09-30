57581e149cffca0f3e4438c28b1c41cf3ba199c3
Milan Kacálek <kacalek@oksystem.cz>
2016-09-30 13:08:09 +0200

    Přidany testy.

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 apk add --no-cache curl
-npm install -f json-diff
+npm install -d json-diff
 
 curl "http://hndocker.oksystem.local:58081/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o a.json
 curl "http://hndocker.oksystem.local:58081/message/get/0018B2000000033A?token=kBPIDfNdSfk8fkATerBa6ct6yshdPbOX&order=desc&start=2014-01-01T01%3A01%3A01&limit=10" -o b.json
