5ce7757fab679e3b6b5993160541c9d382e9ff68
Milan Kacálek <kacalek@oksystem.cz>
2016-11-10 07:37:36 +0100

    pokus pro ci.

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -3,13 +3,8 @@
 set -e -x
 
 ls -l
-npm install
-npm run typings install
-npm run grunt
-ls -l
 cp -R ./bin ../binaries/
 cp -R ./docs ../binaries/
-cp -R ./dist ../binaries/
 cp ./config.yaml ../binaries/
 cp ./package.json ../binaries/
 cp ./Dockerfile ../binaries/
